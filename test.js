051402c853ee2679f8a31efb2ec6ef1261a737f4
Azizbek Aslanov <95647896+itsaslanov@users.noreply.github.com>
2026-05-17 18:33:31 +0900

    updated

--- test.js
+++ test.js
@@ -3,10 +3,10 @@ import { sleep } from "k6";
 
 export const options = {
   vus: 2000, // slowly?
-  duration: "20s",
+  duration: "10s",
 };
 
 export default function () {
-  http.get("web site url?");
+  http.get("https://www.aziz.kr/");
   sleep(1);
 }
